14bc2062b85d2b267e47fa0878cf98e71aed31c2
Kempis <kempisguerrero96@gmail.com>
2023-06-09 19:57:51 -0500

    Vercel analytics integration.

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '@/styles/globals.scss';
 import { SessionProvider } from 'next-auth/react';
 import { CharacterProvider } from '@/contexts/CharacterContext';
+import { Analytics } from '@vercel/analytics/react';
 
 function App({ Component, pageProps, session }) {
   return (
     <SessionProvider session={session}>
       <CharacterProvider>
         <Component {...pageProps} />
+        <Analytics />
       </CharacterProvider>
     </SessionProvider>
   );
